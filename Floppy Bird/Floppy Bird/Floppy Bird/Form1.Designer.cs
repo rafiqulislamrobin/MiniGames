166685224026064b1746356665073e771d8ed111
rafiqulislamrobin <55344358+rafiqulislamrobin@users.noreply.github.com>
2021-04-05 01:24:39 +0600

    Add files via upload

---
+++ Floppy Bird/Floppy Bird/Floppy Bird/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace Floppy_Bird
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FloppyBird = new System.Windows.Forms.PictureBox();
+            this.pipeBottom1 = new System.Windows.Forms.PictureBox();
+            this.pipeTop2 = new System.Windows.Forms.PictureBox();
+            this.pipeTop1 = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipeBottom2 = new System.Windows.Forms.PictureBox();
+            this.scoreTxt = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.StartMenu = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.FloppyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FloppyBird
+            // 
+            this.FloppyBird.Image = global::Floppy_Bird.Properties.Resources.flappyBird1;
+            this.FloppyBird.Location = new System.Drawing.Point(12, 326);
+            this.FloppyBird.Name = "FloppyBird";
+            this.FloppyBird.Size = new System.Drawing.Size(71, 44);
+            this.FloppyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FloppyBird.TabIndex = 0;
+            this.FloppyBird.TabStop = false;
+            // 
+            // pipeBottom1
+            // 
+            this.pipeBottom1.Image = global::Floppy_Bird.Properties.Resources.pipeBottom1;
+            this.pipeBottom1.Location = new System.Drawing.Point(728, 342);
+            this.pipeBottom1.Name = "pipeBottom1";
+            this.pipeBottom1.Size = new System.Drawing.Size(76, 262);
+            this.pipeBottom1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom1.TabIndex = 1;
+            this.pipeBottom1.TabStop = false;
+            // 
+            // pipeTop2
+            // 
+            this.pipeTop2.Image = global::Floppy_Bird.Properties.Resources.pipeTop2;
+            this.pipeTop2.Location = new System.Drawing.Point(1103, -8);
+            this.pipeTop2.Name = "pipeTop2";
+            this.pipeTop2.Size = new System.Drawing.Size(76, 225);
+            this.pipeTop2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop2.TabIndex = 2;
+            this.pipeTop2.TabStop = false;
+            // 
+            // pipeTop1
+            // 
+            this.pipeTop1.Image = global::Floppy_Bird.Properties.Resources.pipeTop1;
+            this.pipeTop1.Location = new System.Drawing.Point(473, 1);
+            this.pipeTop1.Name = "pipeTop1";
+            this.pipeTop1.Size = new System.Drawing.Size(76, 216);
+            this.pipeTop1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop1.TabIndex = 3;
+            this.pipeTop1.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::Floppy_Bird.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(0, 569);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(1467, 75);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 4;
+            this.ground.TabStop = false;
+            // 
+            // pipeBottom2
+            // 
+            this.pipeBottom2.Image = global::Floppy_Bird.Properties.Resources.pipeBottom2;
+            this.pipeBottom2.Location = new System.Drawing.Point(1303, 342);
+            this.pipeBottom2.Name = "pipeBottom2";
+            this.pipeBottom2.Size = new System.Drawing.Size(76, 236);
+            this.pipeBottom2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom2.TabIndex = 5;
+            this.pipeBottom2.TabStop = false;
+            // 
+            // scoreTxt
+            // 
+            this.scoreTxt.AutoSize = true;
+            this.scoreTxt.BackColor = System.Drawing.Color.Black;
+            this.scoreTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scoreTxt.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.scoreTxt.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.scoreTxt.Location = new System.Drawing.Point(664, 615);
+            this.scoreTxt.Name = "scoreTxt";
+            this.scoreTxt.Size = new System.Drawing.Size(82, 27);
+            this.scoreTxt.TabIndex = 6;
+            this.scoreTxt.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AccessibleRole = System.Windows.Forms.AccessibleRole.ButtonMenu;
+            this.menuStrip1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StartMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 615);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1449, 29);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Start);
+            // 
+            // StartMenu
+            // 
+            this.StartMenu.BackColor = System.Drawing.Color.Black;
+            this.StartMenu.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.StartMenu.ForeColor = System.Drawing.Color.White;
+            this.StartMenu.Name = "StartMenu";
+            this.StartMenu.Size = new System.Drawing.Size(57, 25);
+            this.StartMenu.Text = "Start";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1449, 644);
+            this.Controls.Add(this.scoreTxt);
+            this.Controls.Add(this.pipeTop1);
+            this.Controls.Add(this.pipeTop2);
+            this.Controls.Add(this.FloppyBird);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeBottom1);
+            this.Controls.Add(this.pipeBottom2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1465, 683);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1465, 683);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Floppy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.FloppyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox FloppyBird;
+        private System.Windows.Forms.PictureBox pipeBottom1;
+        private System.Windows.Forms.PictureBox pipeTop2;
+        private System.Windows.Forms.PictureBox pipeTop1;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox pipeBottom2;
+        private System.Windows.Forms.Label scoreTxt;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem StartMenu;
+    }
+}
+
---
+++ Floppy Bird/Floppy Bird/Floppy Bird/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Floppy_Bird
+{
+    public partial class Form1 : Form
+    {
+
+        int pipeSpeed = 8;
+        int gravity = 10;
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            GameStart();
+
+        }
+        private void GameStart()
+        {
+
+            gameTimer.Enabled = true;
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            FloppyBird.Top += gravity;
+            pipeBottom1.Left -= pipeSpeed;
+            pipeTop1.Left -= pipeSpeed;
+            pipeBottom2.Left -= pipeSpeed;
+            pipeTop2.Left -= pipeSpeed;
+
+            scoreTxt.Text = $"Score: {score.ToString()}";
+            if (pipeBottom1.Left < -140)
+            {
+                pipeBottom1.Left = 1650;
+                score++;
+            }
+            if (pipeBottom2.Left < -130)
+            {
+                pipeBottom2.Left = 1649;
+                score++;
+            }
+            if (pipeTop1.Left < -120)
+            {
+                pipeTop1.Left = 1550;
+                score++;
+            }
+            if (pipeTop2.Left < -110)
+            {
+                pipeTop2.Left = 1599;
+                score++;
+            }
+            if (score > 10)
+            {
+                pipeSpeed = 15;
+            }
+            if (score > 50)
+            {
+                pipeSpeed = 20;
+            }
+            if (score > 50)
+            {
+                pipeSpeed = 25;
+            }
+
+
+            if (FloppyBird.Bounds.IntersectsWith(pipeBottom1.Bounds) ||
+               FloppyBird.Bounds.IntersectsWith(pipeBottom2.Bounds) ||
+               FloppyBird.Bounds.IntersectsWith(pipeTop1.Bounds) ||
+               FloppyBird.Bounds.IntersectsWith(pipeTop2.Bounds) ||
+               FloppyBird.Bounds.IntersectsWith(ground.Bounds) ||
+               FloppyBird.Top < -22)
+
+            {
+                EndGame();
+            }
+        }
+
+        private void gameKeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -10;
+
+            }
+        }
+
+        private void gameKeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 10;
+            }
+        }
+        private void EndGame()
+        {
+            gameTimer.Stop();
+            scoreTxt.Text += "   Game Over!!!";
+        } 
+
+
+       
+
+        private void Start(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Process.Start(Application.ExecutablePath);
+
+            // Closes the current process
+            Environment.Exit(0);
+            
+        }
+    }
+}
